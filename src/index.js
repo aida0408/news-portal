a5910ab6428b9bd37ae9ee843bfda7d2bf1cae86
Bakazova Aida <solnyshko669@gmail.com>
2021-10-13 21:58:48 +0600

    first

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "./assets/styles/main.css"
+
+
+
+
+ReactDOM.render(
+
+        <App />
+       ,
+  document.getElementById('root')
+);
+
+
